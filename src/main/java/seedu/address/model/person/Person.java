590188a1cd286defaac26aad2dae7db54007103a
CaLraith <hm.leewenxin@gmail.com>
2026-03-09 13:07:28 +0800

    Change duplicate detection to exclude name and include email

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -71,8 +71,8 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName())
-                && otherPerson.getPhone().equals(getPhone());
+                && (otherPerson.getPhone().equals(getPhone())
+                || otherPerson.getEmail().equals(getEmail()));
     }
 
     /**
